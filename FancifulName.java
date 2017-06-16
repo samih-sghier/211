ae33e3270e3240f53b950036cf2b56fa52c76930
samih-sghier <samih.sghier97@gmail.com>
2017-06-15 19:39:10 -0500

    Add files via upload

---
+++ FancifulName.java
@@ -0,0 +1,67 @@
+
+public class FancifulName {
+
+	public static void main(String[] args) {
+		displayS();
+		displayA();
+		displayM();
+		displayI();
+		displayH();
+	System.out.println("-----------------");
+	displayH();
+	displayI();
+	displayM();
+	
+		
+		
+
+	}
+	public static void displayS(){
+		System.out.println("     SSSS");
+		System.out.println("    SS");
+		System.out.println("   SS");
+		System.out.println("      SS");
+		System.out.println("        SS");
+		System.out.println("       SS");
+		System.out.println("    SSSS");
+		System.out.println("                ");
+	}
+	public static void displayA(){
+		System.out.println("    AA");
+		System.out.println("   A  A");
+		System.out.println("  AAAAAA");
+		System.out.println("  A     A");
+		System.out.println("  A     A");
+		System.out.println("              ");
+		
+	}
+	public static void displayM(){
+		System.out.println("  M      M  ");
+		System.out.println("  M M  M M");
+		System.out.println("  M   M  M");
+		System.out.println("  M      M");
+		System.out.println("  M      M");
+		System.out.println("                 ");
+	
+		
+	}
+	public static void displayI(){
+		System.out.println("   IIIII");
+		System.out.println("     I");
+		System.out.println("     I");
+		System.out.println("     I");
+		System.out.println("     I");
+		System.out.println("   IIIII");
+		System.out.println("               ");
+		
+	}
+	public static void displayH(){
+		System.out.println("  H       H");
+		System.out.println("  H       H");
+		System.out.println("  HHHHHHHHH");
+		System.out.println("  H       H");
+		System.out.println("  H       H");
+		
+		
+	}
+}
